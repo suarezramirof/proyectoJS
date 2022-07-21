0213d0def0172af97aa656887b3d4852a625860f
Ramiro Suarez <suarezramirof@gmail.com>
2022-07-21 18:47:10 -0300

    Correcciones

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 let periodo;
 while (periodo != "exit") {
-    const montoInicial = parseInt(prompt("Ingrese el monto inicial (número entero)"));
-    if (montoInicial
-    const cantidadAnios = parseInt(prompt("Ingrese el tiempo en años"));
-    const rendimientoAnual = parseFloat(prompt("Ingrese el rendimiento anual en porcentaje"));
+    periodo = 0;
+    let montoInicial, cantidadAnios, rendimientoAnual;
+    do {
+        montoInicial = parseInt(prompt("Ingrese el monto inicial (número entero)"));
+        if (!montoInicial) {
+            alert("Ingrese un número válido");
+        }
+    } while (!montoInicial);
+    do {
+        cantidadAnios = parseInt(prompt("Ingrese el tiempo en años"));
+        if (!cantidadAnios) {
+            alert("Ingrese un número válido");
+        }
+    } while (!cantidadAnios);
+    do {
+        rendimientoAnual = parseFloat(prompt("Ingrese el rendimiento anual en porcentaje"));
+        if (!rendimientoAnual) {
+            alert("Ingrese un número válido");
+        }
+    } while (!rendimientoAnual);
     // El periodo se pide dentro de otro bucle, para que se pueda modificar sin necesidad de cargar nuevamente las otras variables.
     while (periodo != "q" && periodo != "exit") {
-        periodo = prompt("Ingrese el período de interés compuesto en semanas. Para modificar las variables ingrese [q], para salir ingrese [exit]");
-        alert("El monto final será de: " + interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo));
+        do {
+            periodo = (prompt("Ingrese el período de interés compuesto en semanas. Para modificar las variables ingrese [q], para salir ingrese [exit]"));
+            if (!parseInt(periodo) && periodo != "q" && periodo != "exit") {
+                alert("Ingrese un número válido");
+            } else if (periodo == "exit") {
+                alert("Gracias por utilizar la app");
+            }
+        } while (!parseInt(periodo) && periodo != "q" && periodo != "exit");
+        if (periodo != "q" && periodo != "exit") {
+            periodo = parseInt(periodo);
+            alert("El monto final será de: " + interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo));
+        }
     }
 }
 //Función que devuelve el monto final a partir de: un monto inicial, un tiempo en años, un rendimiento anual y un período de interés compuesto.
