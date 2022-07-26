e6c87d3fd85b972d299c9737df12c8d521bab018
Ramiro Suarez <suarezramirof@gmail.com>
2022-07-26 18:59:45 -0300

    Cambios para el almacenamiento de variables y resultados en una estructura de datos y posterior devolución de los mismos a la consola

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let periodo;
+// Array en el que se van a cargar los resultados (y las variables)
+const resultados = [];
 while (periodo != "exit") {
     periodo = 0;
     let montoInicial, cantidadAnios, rendimientoAnual;
@@ -22,6 +24,8 @@ while (periodo != "exit") {
             alert("Ingrese un número válido");
         }
     } while (!rendimientoAnual);
+    // Se define un objeto con propiedades que corresponden al input que se pidió al usuario, y asignando a las mismas los valores que fueron introducidos. A la última propiedad, casos, se le asigna un array vacío para luego cargar en el mismo los resultados de los cálculos.
+    let variables = {"Monto inicial": montoInicial, "Años de inversión": cantidadAnios, "Rendimiento anual": rendimientoAnual, "Casos": []};
     // El periodo se pide dentro de otro bucle, para que se pueda modificar sin necesidad de cargar nuevamente las otras variables.
     while (periodo != "q" && periodo != "exit") {
         //Mismo mecanismo que en los anteriores, pero la condición para repetir el bucle es que la conversión a entero no se pueda realizar Y que sea diferente de [q] o [exit]
@@ -31,14 +35,21 @@ while (periodo != "exit") {
             if (!parseInt(periodo) && periodo != "q" && periodo != "exit") {
                 alert("Ingrese un número válido");
             } else if (periodo == "exit") {
+                // La línea inferior obedece a que la función se ejecuta dentro del bucle, el "push" que está por fuera se ejecuta posteriormente
+                resultados.push(variables);
+                // Se muestran los resultados en la consola. 
+                console.log(mostrarResultados(resultados));
                 alert("Gracias por utilizar la app");
             }
         } while (!parseInt(periodo) && periodo != "q" && periodo != "exit");
         if (periodo != "q" && periodo != "exit") {
             periodo = parseInt(periodo);
-            alert("El monto final será de: " + interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo));
+            let montoFinal = interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo);
+            alert("El monto final será de: " + montoFinal);
+            variables["Casos"].push({periodo: periodo, montoFinal: montoFinal});
         }
     }
+    resultados.push(variables);
 }
 //Función que devuelve el monto final a partir de: un monto inicial, un tiempo en años, un rendimiento anual y un período de interés compuesto.
 function interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo) {
@@ -55,3 +66,28 @@ function interesCompuesto(montoInicial,cantidadAnios,rendimientoAnual,periodo) {
     }
     return montoFinal;
 }
+//Función que devuelve una cadena de texto formateada, a partir de las variables introducidas y los resultados calculados
+function mostrarResultados(resultados) {
+    let informe = "";
+    // El largo del array "resultados" es igual a la cantidad de veces que se carguen variables de inicio.
+    for (let i = 0; i < resultados.length; i++) {
+        // Título para cada caso
+        informe = informe + "\nCaso " + (i+1) + "\n";
+        for (propiedad of Object.keys(resultados[i])) {
+            if (propiedad == "Casos") {
+                // La propiedad "Casos" de un objeto dado en el array "Resultados", tiene como valor un array cuyo largo es igual a la cantidad de períodos diferentes que se hayan ingresado.
+                for (let j = 0; j < resultados[i][propiedad].length; j++) {
+                    informe = informe + "\n- Período de interés compuesto = " + resultados[i][propiedad][j]["periodo"] + " semanas\n";
+                    informe = informe + "- Monto final = " + resultados[i][propiedad][j]["montoFinal"] + "\n";
+                }
+            // Las otras propiedades se pasan a la cadena con su nombre sin modificar, al igual que los valores. En el caso de la propiedad "Rendimiento anual", se agrega un % al final de la cadena.
+            } else if (propiedad == "Rendimiento anual") {
+                informe = informe + `${propiedad} = ${resultados[i][propiedad]}` + "%\n";
+            } else {
+                informe = informe + `${propiedad} = ${resultados[i][propiedad]}` + "\n";
+            }
+        }
+    }
+    return informe;
+}
+ 
